07d6ffb5506017915d3f64aae6db4e03ff03009c
web-source-dev <muhammadnouman72321@gmail.com>
2024-11-20 12:36:09 +0500

    Update question.js

--- models/question.js
+++ models/question.js
@@ -1,1 +1,15 @@
 
+const mongoose = require('mongoose');
+
+const questionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        default: 'What is the name of your childhood friend'
+    },
+    correct_answer: {
+        type: String,
+        default:"harmony"
+    }
+})
+
+module.exports = mongoose.model('Question', questionSchema);
